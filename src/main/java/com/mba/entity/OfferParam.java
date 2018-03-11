5332f0c2d82df26989748d13015cb0dddd08701b
Mohammad B. A. M. Abdelhadi <m.bassam@GMG.LOCAL>
2018-03-11 20:32:29 +0400

    fixing after merge

--- src/main/java/com/mba/entity/OfferParam.java
+++ src/main/java/com/mba/entity/OfferParam.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package com.mba.entity;
 
 /**
@@ -35,41 +34,4 @@ public enum OfferParam {
 		return param;
 	}
 }
-=======
-package com.mba.entity;
 
-/**
- * Enum having the possible param required for getOffers to pass them in url.
- * @author m.bassam
- *
- */
-public enum OfferParam {
-	
-	METHOD("getOffers"), 
-	SCENARIO("scenario"), 
-	PAGE("page"), 
-	UID("uid"), 
-	PRODUCT_TYPE("productType"),
-	DESTINACTION_NAME("destinationName"),  
-	MIN_TRIP_START_DATE("minTripStartDate"),
-	MAX_TRIP_START_DATE("maxTripStartDate"),
-	MIN_STAR_RATING("minStarRating"),
-	MAX_STAR_RATING("maxStarRating"),
-	MIN_TOTAL_RATE("minTotalRate"),
-	MAX_TOTAL_RATE("maxTotalRate");
-	
-	/**
-	 * Instance variable  
-	 */
-	
-	private String param;
-
-	OfferParam(String param) {
-		this.param = param; 
-	}
-
-	public String param() {
-		return param;
-	}
-}
->>>>>>> a8c9a4e16b20a6c0eb2bb5200f21f3f535dafbce
